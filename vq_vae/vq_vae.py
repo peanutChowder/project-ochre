5959398b3f65269a9e57179ad6ed25e4b2a29e43
Jacob Feng <peanutchowder@gmail.com>
2025-12-26 22:55:15 -0800

    Fixed old import in vqvae too

--- vq_vae/vq_vae.py
+++ vq_vae/vq_vae.py
@@ -15,7 +15,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-from torch.amp.grad_scaler import GradScaler
+from torch.cuda.amp import GradScaler
 from torchvision import transforms, utils
 
 try:
