35d159b27cdf3580d8efe954b3dfe696d61457ea
Jacob Feng <peanutchowder@gmail.com>
2026-01-13 11:24:50 -0700

    Fixed paths

--- train.py
+++ train.py
@@ -23,8 +23,8 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print("Device:", DEVICE)
 
 # --- PATHS ---
-DATA_DIR = "./preprocessedv5"
-VQVAE_PATH = "./vq_vae/checkpoints/vqvae_v2.1.6__epoch100.pt" 
+DATA_DIR = "../preprocessedv5"
+VQVAE_PATH = "./checkpoints/vqvae_v2.1.6__epoch100.pt" 
 MANIFEST_PATH = os.path.join(DATA_DIR, "manifest.json")
 
 # --- HYPERPARAMETERS ---
